abaa22372a0e8e84fb837fa1502963962f44db04
asif-4u <106628177+asif-4u@users.noreply.github.com>
2022-09-15 22:47:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,8 @@ const App = () => {
   }
   return (
     <div id="main">
-      <button id="button-a">Button A</button>
+      <button id="button-a" onClick={clickA}>Button A</button>
+    
     </div>
   )
 }
